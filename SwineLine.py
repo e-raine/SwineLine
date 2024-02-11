ca513332aa666f04d862a93fe255fa915b0e5e83
Eraine <el.rain14@gmail.com>
2024-02-11 14:58:36 +0800

    SwineLine

--- SwineLine.py
+++ SwineLine.py
@@ -1,15 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="SwineLine"
+)
 # Title
 st.title("Swineline")
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility:hidden;}
-            header {visibility:hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # header
 st.header("This is the Heading")
