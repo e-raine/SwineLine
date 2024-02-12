72a20005fd1a675cc632cd7bb91fb83ff2271b3a
Eraine <el.rain14@gmail.com>
2024-02-12 17:23:03 +0800

    SwineLine

--- SwineLine.py
+++ SwineLine.py
@@ -8,22 +8,4 @@ st.set_page_config(
 )
 
 
-
-# Title
-st.title("Swineline")
-
-# header
-st.header("This is the Heading")
-
-# subheader
-st.subheader("This is the subheading")
-
-# text
-st.text("Hello World!")
-
-# success, info
-st.success("Executed successfully")
-st.info("This is an information")
-st.warning("This is a warning")
-st.error("This is an error")
-
+st.title("SwineLine")
