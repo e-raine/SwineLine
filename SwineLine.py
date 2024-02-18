e22621570f4d2aa981d66f4f57f44e07ce13f315
Eraine <el.rain14@gmail.com>
2024-02-18 13:38:24 +0800

    SwineLine

--- SwineLine.py
+++ SwineLine.py
@@ -1,11 +1,10 @@
-from PIL import Image
 import streamlit as st
+from PIL import Image
 
-
+# Changing the web app logo and title
 st.set_page_config(
     page_title="SwineLine",
     page_icon= Image.open("favicon.ico")
 )
-
 
 st.title("SwineLine")
