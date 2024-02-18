ac04dc18a8467ad84f344c424d47c0f210ea6335
Eraine <el.rain14@gmail.com>
2024-02-19 00:25:11 +0800

    SwineLine

--- SwineLine.py
+++ SwineLine.py
@@ -10,7 +10,6 @@ st.set_page_config(
 
 st.markdown("""
 <header tabindex="-1" data-testid="stHeader" class="st-emotion-cache-1avcm0n ezrtsby2">
-    <nav>
         <ul>
             <li>
                 <div class="dropdown" style="float:left;">
@@ -22,13 +21,14 @@ st.markdown("""
                 </div>
             </li>
             <li>
-                <a href="#">
-                <img src="https://pouch.jumpshare.com/preview/ChiIa6vGXXlLBjZXqbOW1g2plJKWohUVtOI1RC2UXR6aIjx8njIQ24kZToBnWgMAyFb93RkyHQzbjCH_oylQni--7bgpO1xY3C4xkXUfm4s" style="width: 3.5rem; height: 3.5rem;">
-                <img src="https://pouch.jumpshare.com/preview/UI--6dHDTuYM1qQqFxI1F1dwpmwZrLF8kgGjIPH7QklLllPYduKB_fjGKMEuM-5-Fyh0q_s75f28xrrTzp5nT1IwJ-AR6DFQHBOxFbY-Kg4" style="width: 8rem; height: 4.5rem;">
-                </a>
+                <div class="logo">
+                    <a href="#">
+                    <img src="https://pouch.jumpshare.com/preview/ChiIa6vGXXlLBjZXqbOW1g2plJKWohUVtOI1RC2UXR6aIjx8njIQ24kZToBnWgMAyFb93RkyHQzbjCH_oylQni--7bgpO1xY3C4xkXUfm4s" style="width: 3.5rem; height: 3.5rem;">
+                    <img src="https://pouch.jumpshare.com/preview/UI--6dHDTuYM1qQqFxI1F1dwpmwZrLF8kgGjIPH7QklLllPYduKB_fjGKMEuM-5-Fyh0q_s75f28xrrTzp5nT1IwJ-AR6DFQHBOxFbY-Kg4" style="width: 8rem; height: 4.5rem;">
+                    </a>
+                </div>
             </li>
         </ul>
-    </nav>
 </header>
             
 """, unsafe_allow_html=True)
