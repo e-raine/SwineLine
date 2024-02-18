62708c15bb1ec98410e24f9817f09ec596fb1d9f
Eraine <el.rain14@gmail.com>
2024-02-18 20:04:25 +0800

    SwineLine

--- SwineLine.py
+++ SwineLine.py
@@ -14,7 +14,7 @@ st.markdown("""
   <ul>
   <li>
     <a href="#">
-        <img src="https://scontent.fcgm1-1.fna.fbcdn.net/v/t39.30808-6/426405471_122097138446217431_8065067454837553768_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeFIeH8sRiafbnX58D-3zpvPUDm-yH6FMftQOb7IfoUx-3ZVVQMVujEsad8YjO5eL1ssYD8yUwYgdf0RYRlG_IY7&_nc_ohc=xnaQJVlKHcEAX-L4iZh&_nc_ht=scontent.fcgm1-1.fna&oh=00_AfBBgzJDNDaO150NeZnh-qLw9-eSm3knobjPc6dABwne8A&oe=65D5F92C">
+        <img src="https://drive.google.com/file/d/1rhy0E-lIQoefG7KE5Ife6YO51MQw6vCr/view?usp=drive_link">
     </a>
   </li>
   </ul>
