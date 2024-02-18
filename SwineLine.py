9e53bb02380ddff7ff5ef7c175e5c9178809d874
Eraine <el.rain14@gmail.com>
2024-02-18 19:29:02 +0800

    SwineLine

--- SwineLine.py
+++ SwineLine.py
@@ -7,23 +7,13 @@ st.set_page_config(
     page_icon= Image.open("favicon.ico")
 )
 
-st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
 <header tabindex="-1" data-testid="stHeader" class="st-emotion-cache-1avcm0n ezrtsby2">
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="images\SVG\Swineline Header_Pink.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      Bootstrap
-    </a>
-  </div>
-</nav>
+    <a href="#" class="logo"><img src="images\favicon-16x16.png"></a>
 </header>
 """, unsafe_allow_html=True)
 
 # connect with css file
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-st.markdown('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>', unsafe_allow_html=True)
